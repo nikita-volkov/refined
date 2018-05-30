0442f9516978e402c11d8ce83bbd6c7587dce490
chessai <chessai1996@gmail.com>
2018-05-29 20:53:02 -0400

    builds with GHC >= 8.0

--- release.nix
+++ release.nix
@@ -1,5 +1,4 @@
 {
-    ghc7103 = import ./default.nix { compiler = "ghc7103"; };
     ghc802  = import ./default.nix { compiler = "ghc802";  };
     ghc822  = import ./default.nix { compiler = "ghc822";  };
     ghc842  = import ./default.nix { compiler = "ghc842";  };  
