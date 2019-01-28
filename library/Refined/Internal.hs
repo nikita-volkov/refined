4bc2807e98d81b56268388683ea9d154657fc5d0
chessai <chessai1996@gmail.com>
2019-01-28 16:04:09 -0500

    add tojson/fromjson/arbitrary instances

--- library/Refined/Internal.hs
+++ library/Refined/Internal.hs
@@ -185,7 +185,7 @@ import qualified Control.Monad.Trans.Except   as ExceptT
 import           GHC.Generics                 (Generic, Generic1)
 import           GHC.TypeLits                 (type (<=), KnownNat, Nat, natVal)
 
-import           Refined.These                (These(This,That,These))
+import           Data.These                   (These(This,That,These))
 
 import qualified Data.Text.Prettyprint.Doc    as PP
 import qualified Language.Haskell.TH.Syntax   as TH
