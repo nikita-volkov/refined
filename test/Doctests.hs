9c667876d7d6561414b0cf7d4ee2bd19c13f70e9
chessai <chessai1996@gmail.com>
2020-07-11 18:06:14 -0700

    remove old modules from doctests

--- test/Doctests.hs
+++ test/Doctests.hs
@@ -8,10 +8,6 @@ main = doctest $ srcFiles ++ compFlags
 srcFiles :: [String]
 srcFiles =
   [ "src/Refined.hs"
-  , "src/Refined/Internal.hs"
-  , "src/Refined/Orphan/Aeson.hs"
-  , "src/Refined/Orphan/QuickCheck.hs"
-  , "src/Refined/Orphan.hs"
   , "src/Refined/These.hs"
   , "src/Refined/Unsafe/Type.hs"
   , "src/Refined/Unsafe.hs"
