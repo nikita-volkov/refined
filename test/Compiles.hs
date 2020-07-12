5d4ea2c1e177c2f203070b051c00f55f06528f70
chessai <chessai1996@gmail.com>
2020-07-11 20:09:53 -0700

    remove unnecessary stuff in test/Compiles.hs

--- test/Compiles.hs
+++ test/Compiles.hs
@@ -10,16 +10,11 @@
 module Main (main) where
 
 import Refined
-import Prelude (IO,putStrLn,Int)
 import Data.Void (Void)
 
 main :: IO ()
 main = do
   putStrLn "refined/test/Compiles.hs: it compiles!"
-
-id   = $$(refineTH @IdPred     @Int 3)
-even = $$(refineTH @(Not Even) @Int 3)
-odd  = $$(refineTH @Odd        @Int 3)
 
 id_   = $$(refineTH_ @IdPred     @Int 3)
 even_ = $$(refineTH_ @(Not Even) @Int 3)
